0d9f4bc80614d876396c3fbd30678e40b2c7a4c2
Cribest7890 <mandacristian13@gmail.com>
2026-08-27 19:21:00 +0200

    Update input_unix.go

--- input/input_unix.go
+++ input/input_unix.go
@@ -6,9 +6,12 @@ import (
 	"os"
 	"syscall"
 	"unsafe"
+
+	"golang.org/x/sys/unix" // Pacchetto standard per le costanti di sistema Unix
 )
 
-var origTerm syscall.Termios
+// Cambiamo il tipo in unix.Termios per compatibilità cross-platform
+var origTerm unix.Termios
 
 func Setup() error {
 	stateMutex.Lock()
@@ -19,20 +22,24 @@ func Setup() error {
 	}
 
 	fd := int(os.Stdin.Fd())
-	if _, _, err := syscall.Syscall6(syscall.SYS_IOCTL, uintptr(fd), uintptr(syscall.TCGETS), uintptr(unsafe.Pointer(&origTerm)), 0, 0, 0); err != 0 {
+	
+	// Usiamo unix.SYS_IOCTL e unix.TCGETS per supportare Linux
+	if _, _, err := syscall.Syscall6(unix.SYS_IOCTL, uintptr(fd), uintptr(unix.TCGETS), uintptr(unsafe.Pointer(&origTerm)), 0, 0, 0); err != 0 {
 		return err
 	}
 
 	raw := origTerm
-	raw.Iflag &^= syscall.IGNBRK | syscall.BRKINT | syscall.PARMRK | syscall.ISTRIP | syscall.INLCR | syscall.IGNCR | syscall.ICRNL | syscall.IXON
-	raw.Oflag &^= syscall.OPOST
-	raw.Lflag &^= syscall.ECHO | syscall.ECHONL | syscall.ICANON | syscall.ISIG | syscall.IEXTEN
-	raw.Cflag &^= syscall.CSIZE | syscall.PARENB
-	raw.Cflag |= syscall.CS8
-	raw.Cc[syscall.VMIN] = 1
-	raw.Cc[syscall.VTIME] = 0
+	// Modifichiamo i flag usando le costanti standard del pacchetto unix
+	raw.Iflag &^= unix.IGNBRK | unix.BRKINT | unix.PARMRK | unix.ISTRIP | unix.INLCR | unix.IGNCR | unix.ICRNL | unix.IXON
+	raw.Oflag &^= unix.OPOST
+	raw.Lflag &^= unix.ECHO | unix.ECHONL | unix.ICANON | unix.ISIG | unix.IEXTEN
+	raw.Cflag &^= unix.CSIZE | unix.PARENB
+	raw.Cflag |= unix.CS8
+	raw.Cc[unix.VMIN] = 1
+	raw.Cc[unix.VTIME] = 0
 
-	if _, _, err := syscall.Syscall6(syscall.SYS_IOCTL, uintptr(fd), uintptr(syscall.TCSETSW), uintptr(unsafe.Pointer(&raw)), 0, 0, 0); err != 0 {
+	// Usiamo unix.TCSETSW per applicare le modifiche dopo che l'output è stato trasmesso
+	if _, _, err := syscall.Syscall6(unix.SYS_IOCTL, uintptr(fd), uintptr(unix.TCSETSW), uintptr(unsafe.Pointer(&raw)), 0, 0, 0); err != 0 {
 		return err
 	}
 
@@ -60,11 +67,11 @@ func Restore() error {
 	}
 
 	fd := int(os.Stdin.Fd())
-	if _, _, err := syscall.Syscall6(syscall.SYS_IOCTL, uintptr(fd), uintptr(syscall.TCSETSW), uintptr(unsafe.Pointer(&origTerm)), 0, 0, 0); err != 0 {
+	// Ripristiniamo lo stato originale usando unix.TCSETSW
+	if _, _, err := syscall.Syscall6(unix.SYS_IOCTL, uintptr(fd), uintptr(unix.TCSETSW), uintptr(unsafe.Pointer(&origTerm)), 0, 0, 0); err != 0 {
 		return err
 	}
 
 	isSetup = false
 	return nil
 }
-
